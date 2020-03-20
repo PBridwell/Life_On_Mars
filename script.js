67b77e682036191fd6dea4b1b81c7719e5caa094
mwturner611 <mwturner611@gmail.com>
2020-03-19 21:26:50 -0500

    select specific values from ajax call

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ $.ajax({
   url: queryURLWeather,
   method: "GET"
 }).then(function(response){
-    console.log(response);
+  
+  //get object of available sols 
+  var sols = response.sol_keys;
+
+  // call 7 sols available DATE-SEASON-TEMP-AM
+  for (var i = 0; i < sols.length; i++){
+    console.log("Date: " + (response[sols[i]].Last_UTC).slice(0,10));
+    console.log("Season: " + response[sols[i]].Season);
+    console.log("Degrees in Celsius: " + response[sols[i]].AT.av);
+    console.log("Atmospheric pressure in Pascals: " + response[sols[i]].PRE.av);
+    console.log("Wind speed in metres per second: " + response[sols[i]].HWS.av);
+  }
+
 })
 
 var queryURLImages = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=eEbMOIDyKxBlDGl2ggUdMiMKyzdqwqjDBxMYcLZK";
@@ -18,9 +30,9 @@ $.ajax({
   method: "GET"
 }).then(function(response){
     console.log(response);
-    var photo = $("<img>");
-    photo.attr("src",response.photos[3].img_src);
-    $("#marsPhoto").append(photo);
+    // var photo = $("<img>");
+    // photo.attr("src",response.photos[3].img_src);
+    // $("#marsPhoto").append(photo);
 
 
 
