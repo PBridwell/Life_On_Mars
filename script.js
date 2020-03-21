a8afd10c02736f1db100b512e25c1ffefb37bbd9
P Bridwell <patbridwell@cox.net>
2020-03-21 08:21:04 -0500

    adds images to page and resizes displayed images

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $.ajax({
     console.log(response);
 })
 
-var queryURLImages = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=eEbMOIDyKxBlDGl2ggUdMiMKyzdqwqjDBxMYcLZK";
+var queryURLImages = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-10-30&api_key=eEbMOIDyKxBlDGl2ggUdMiMKyzdqwqjDBxMYcLZK";
 
 // Make the AJAX request to the API - GETs the JSON data at the queryURL.
 // The data then gets passed as an argument to the updatePage function
@@ -19,6 +19,8 @@ $.ajax({
 }).then(function(response){
     console.log(response);
     var photo = $("<img>");
+    photo.attr('width', '250px')
+    photo.attr('height', '250px')
     photo.attr("src",response.photos[3].img_src);
     $("#marsPhoto").append(photo);
 
